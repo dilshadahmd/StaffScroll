b82c7f2caec5bb3bd978d9af229f6e0b47b98e08
dilshad <dilshad4hammed@gmail.com>
2024-06-15 14:34:10 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StaffScroll.DAL;
+using StaffScroll.Models;
+
+namespace StaffScroll.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public EmployeeDbContext _context { get; }
+        public EmployeeController(EmployeeDbContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var employees = _context.Employees.ToList();
+            List<EmployeeViewModel> retList = new List<EmployeeViewModel>();
+            if (employees.Any())
+            {
+                foreach (var item in employees)
+                {
+                    var empData = new EmployeeViewModel
+                    {
+                        Id = item.Id,
+                        FirstName = item.FirstName,
+                        DateOfBirth = item.DateOfBirth,
+                        LastName = item.LastName,
+                        Email = item.Email,
+                        Salary = item.Salary,
+                    };
+                    retList.Add(empData);
+                }
+            }
+            return View(retList);
+        }
+    }
+}
---
+++ DAL/EmployeeDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StaffScroll.Models.Entities;
+
+namespace StaffScroll.DAL
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public virtual DbSet<Employee>Employees { get; set; }
+    }
+}

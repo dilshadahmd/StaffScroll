ebe9891d56b12723a8d3b3b9f40ae91c6c6c51da
dilshad <dilshad4hammed@gmail.com>
2024-06-15 18:05:33 +0530

    Employww Create Screen Implimentation

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using StaffScroll.DAL;
 using StaffScroll.Models;
+using StaffScroll.Models.Entities;
 
 namespace StaffScroll.Controllers
 {
@@ -35,6 +36,43 @@ namespace StaffScroll.Controllers
                 }
             }
             return View(retList);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(EmployeeViewModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var employee = new Employee()
+                    {
+                        FirstName = model.FirstName,
+                        LastName = model.LastName,
+                        Email = model.Email,
+                        DateOfBirth = model.DateOfBirth,
+                        Salary = model.Salary,
+                    };
+                    await _context.Employees.AddAsync(employee);
+                    await _context.SaveChangesAsync();
+                    TempData["successMessage"] = "Created Successfully";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["errorMessage"] = "Model data is not valid";
+                }
+                return View();
+            }
+            catch (Exception ex)
+            {
+                TempData["errorMessage"] = ex.Message;
+                return View();
+            }
         }
     }
 }
